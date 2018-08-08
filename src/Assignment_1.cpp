208aa1dcf1da64cb7a594af4f28aaa9e811279d4
Awesomelotad <b.lovegrove.wsd@gmail.com>
2018-08-08 21:43:50 +1200

    It works! finished code. now needs optimising

--- src/Assignment_1.cpp
+++ src/Assignment_1.cpp
@@ -35,9 +35,7 @@ int main() {
     int encoder_r = 0; // right rotary encoder value
     float roboVoltage; // robots battery voltage
     float roboX = 0.0; // robots X co-ord
-    float roboX_change; // change in X co-ord after movement & recalculation
     float roboY = 0.0; // robots Y co-ord
-    float roboY_change; // change in Yco-ord after movement & recalculation
     float roboH = 0.0; // robots heading
     float roboH_change; // change in heading after movement & recalculation
     float roboH_degrees; // robots heading in degrees
@@ -75,7 +73,7 @@ int main() {
 
     // transmit the initial robot position to the PC
     roboH_degrees = roboH * (180 / PI); // convert robo heading to degrees
-    wixel.printf("%8s,-%8.2f,-%8.2f,-%8.2f,-%8.2f,-%8d,-%8d\r\n", "POS,", roboX, roboY, roboH_degrees, encoder_l, encoder_r); // transmit robot co-ords / data
+    wixel.printf("%8s,-%8.2f,-%8.2f,-%8.2f,-%8.2f,-%8d,-%8d\r\n", "POS,", roboX, roboY, roboH_degrees, encoder_l, encoder_r);
     
     // initialise the encoders
     initEncoder();   
@@ -108,16 +106,9 @@ int main() {
         encoder_l = leftEncoder();
         encoder_r = rightEncoder();
       
-        // display the pulse count values on the LCD
-        ostringstream encoderPulses_l;
-        ostringstream encoderPulses_r;
-        encoderPulses_l << "L=" << encoder_l;
-        encoderPulses_r << "R=" << encoder_r;
-        robot.cls();
-        robot.locate(0, 0);
-        robot.printf(encoderPulses_l.str().c_str());
+        // display the pulse count values on the LCD left value = left encoder, right value = right encoder
         robot.locate(0, 1);
-        robot.printf(encoderPulses_r.str().c_str());
+        robot.printf("%3d  %3d", encoder_l, encoder_r);
 
         // if there were any pulses
         if (encoder_l != 0 || encoder_r != 0) {
@@ -132,14 +123,12 @@ int main() {
 
                 roboR = (0.5 * axle_cm * (encoder_r + encoder_l) / (encoder_r - encoder_l));
                 roboH_change = ((encoder_r - encoder_l) * pulseDistance / axle_cm);
-                roboX_change = (roboR * (sin(roboH + roboH_change) - sin(roboH)));
-                roboY_change = (roboR * (cos(roboH) - cos(roboH + roboH_change)));
+                roboX += (roboR * (sin(roboH + roboH_change) - sin(roboH)));
+                roboY += (roboR * (cos(roboH) - cos(roboH + roboH_change)));
 
             }
-            // calculate the new robot position
+            // calculate the new robot heading
             roboH += roboH_change;
-            roboX += roboX_change;
-            roboY += roboY_change;
 
             // if necessary, adjust the heading
             if (roboH < -PI) {
@@ -154,7 +143,7 @@ int main() {
         }
         // transmit the new robot position + data to the PC
         roboH_degrees = roboH * (180 / PI); // convert robo heading to degrees
-        wixel.printf("%8s,-%8.2f,-%8.2f,-%8.2f,-%8.2f,-%8d,-%8d\r\n", "POS,", roboX, roboY, roboH_degrees, encoder_l, encoder_r); // transmit robot co-ords / data
+        wixel.printf("%8s,-%8.2f,-%8.2f,-%8.2f,-%8.2f,-%8d,-%8d\r\n", "POS,", roboX, roboY, roboH_degrees, encoder_l, encoder_r);
 
     }
 }
